41367a7750aad8e1273a89506c38c59c74fb5a13
Braden Everson <bradeneverson@gmail.com>
2025-03-28 07:46:10 -0500

    You can now create a parser with rules from RNG

--- mao-core/src/parser.rs
+++ mao-core/src/parser.rs
@@ -1,18 +1,61 @@
 //! The parser for generating an abstract syntax tree, built in with conditional rules
 
-use crate::tokenizer::Token;
+use rand::{RngCore, seq::IndexedRandom};
 
+use crate::tokenizer::{Token, TokenTag};
+
+/// All valid statement ends
+pub const VALID_ENDS: &[TokenTag<'static>] = &[
+    TokenTag::Semicolon,
+    TokenTag::Dot,
+    TokenTag::Identifier("done"),
+];
+
+/// It's a 2/3 chance that you need parenthesis
+pub const PAREN_PROB: &[bool] = &[true, true, false];
+
+/// The core parser with dynamic rules
 pub struct Parser<'tok> {
     /// The token stream to parse
     tokens: &'tok [Token<'tok>],
     /// Index into the token stream
     idx: usize,
     /// Dynamic parser rules
-    rules: ParserRules<'tok>,
+    rules: ParserRules,
+}
+
+impl<'tok> Parser<'tok> {
+    /// Creates a new parser from RNG
+    pub fn from_rng<RNG: RngCore>(rng: &mut RNG) -> Self {
+        // Unwrap safety, VALID_ENDS is not empty that would be silly
+        let statements_end_with = *VALID_ENDS.choose(rng).unwrap();
+        // Unwrap safety, PAREN_PROB is not empty that would be silly
+        let parenthesis = *PAREN_PROB.choose(rng).unwrap();
+
+        let rules = ParserRules {
+            statements_end_with,
+            parenthesis,
+        };
+
+        Self {
+            tokens: &[],
+            idx: 0,
+            rules,
+        }
+    }
+
+    /// Assigns a token stream to the parser
+    pub fn with_tokens(mut self, tokens: &'tok [Token<'tok>]) -> Self {
+        self.tokens = tokens;
+
+        self
+    }
 }
 
 /// Dynamic parsing rules that affect how the AST is generated
-pub struct ParserRules<'tok> {
-    pub statements_end_with: Token<'tok>,
+pub struct ParserRules {
+    /// What should a statement end with? (;, ., !, something crazy :O)
+    pub statements_end_with: TokenTag<'static>,
+    /// Should parenthesis exist in if statements, loops and function calls?
     pub parenthesis: bool,
 }
