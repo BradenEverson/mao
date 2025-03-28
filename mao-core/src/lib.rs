c13161c9511517f2741219c3a55e858a34c00a04
BradenEverson <bradeneverson@gmail.com>
2025-03-28 06:49:58 -0500

    Create parser and parser rules

--- mao-core/src/lib.rs
+++ mao-core/src/lib.rs
@@ -1,3 +1,4 @@
 //! The core tokenizer, parser and evaluator for a programming language with dynamically randomized syntax
 
+pub mod parser;
 pub mod tokenizer;
---
+++ mao-core/src/parser.rs
@@ -0,0 +1,18 @@
+//! The parser for generating an abstract syntax tree, built in with conditional rules
+
+use crate::tokenizer::Token;
+
+pub struct Parser<'tok> {
+    /// The token stream to parse
+    tokens: &'tok [Token<'tok>],
+    /// Index into the token stream
+    idx: usize,
+    /// Dynamic parser rules
+    rules: ParserRules,
+}
+
+/// Dynamic parsing rules that affect how the AST is generated
+pub struct ParserRules<'tok> {
+    pub statements_end_with: Token<'tok>,
+    pub parenthesis: bool,
+}
